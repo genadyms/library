f25f5dcdfef2a7bea94cd9fcbfd2e023b28e0acb
genadyms <gennadyms@gmail.com>
2016-05-31 23:50:02 +0300

    add some localization

--- webapp/src/main/java/com/gmazurkevich/training/library/webapp/app/AuthorizedSession.java
+++ webapp/src/main/java/com/gmazurkevich/training/library/webapp/app/AuthorizedSession.java
@@ -1,5 +1,7 @@
 package com.gmazurkevich.training.library.webapp.app;
 
+import java.util.Locale;
+
 import javax.inject.Inject;
 
 import org.apache.wicket.Session;
@@ -10,50 +12,59 @@ import org.apache.wicket.request.Request;
 
 import com.gmazurkevich.training.library.datamodel.UserCredentials;
 import com.gmazurkevich.training.library.service.UserService;
-
+import com.gmazurkevich.training.library.webapp.component.localization.LanguageSelectionComponent;
 
 public class AuthorizedSession extends AuthenticatedWebSession {
-    @Inject
-    private UserService userService;
+	@Inject
+	private UserService userService;
 
-    private UserCredentials loggedUser;
+	private UserCredentials loggedUser;
 
-    private Roles roles;
+	private Roles roles;
 
-    public AuthorizedSession(Request request) {
-        super(request);
-        Injector.get().inject(this);
+	public AuthorizedSession(Request request) {
+		super(request);
+		Injector.get().inject(this);
 
-    }
+	}
 
-    public static AuthorizedSession get() {
-        return (AuthorizedSession) Session.get();
-    }
+	public static AuthorizedSession get() {
+		return (AuthorizedSession) Session.get();
+	}
 
-    @Override
-    public boolean authenticate(final String userName, final String password) {
-        loggedUser = userService.getByNameAndPassword(userName, password);
-        return loggedUser != null;
-    }
+	@Override
+	public boolean authenticate(final String userName, final String password) {
+		loggedUser = userService.getByNameAndPassword(userName, password);
+		return loggedUser != null;
+	}
 
-    @Override
-    public Roles getRoles() {
-        if (isSignedIn() && (roles == null)) {
-            roles = new Roles();
-            roles.addAll(userService.resolveRoles(loggedUser.getId()));
-        }
-        return roles;
-    }
+	@Override
+	public Roles getRoles() {
+		if (isSignedIn() && (roles == null)) {
+			roles = new Roles();
+			roles.addAll(userService.resolveRoles(loggedUser.getId()));
+		}
+		return roles;
+	}
 
-    @Override
-    public void signOut() {
-        super.signOut();
-        loggedUser = null;
-        roles = null;
-    }
+	@Override
+	public void signOut() {
+		super.signOut();
+		loggedUser = null;
+		roles = null;
+	}
 
-    public UserCredentials getLoggedUser() {
-        return loggedUser;
-    }
+	public UserCredentials getLoggedUser() {
+		return loggedUser;
+	}
+
+	@Override
+	public Locale getLocale() {
+		Locale locale = super.getLocale();
+		if (locale == null || !LanguageSelectionComponent.SUPPORTED_LOCALES.contains(locale)) {
+			setLocale(LanguageSelectionComponent.SUPPORTED_LOCALES.get(0));
+		}
+		return super.getLocale();
+	}
 
 }
